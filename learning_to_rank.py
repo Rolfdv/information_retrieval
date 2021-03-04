f43d660263d65461a619997427ab1eb7325f5b69
Jip Rietveld <J.A.C.Rietveld@student.tudelft.nl>
2021-03-04 16:39:16 +0100

    Learning to rank models

--- learning_to_rank.py
+++ learning_to_rank.py
@@ -1,4 +1,5 @@
 
+# Please ensure bin/RankLib-2.15.jar is present.
 # Useful website for running: https://sourceforge.net/p/lemur/wiki/RankLib%20How%20to%20use/
-example_query = "java -jar bin/RankLib-2.15.jar -train bin/MQ2008/Fold1/train.txt -test bin/MQ2008/Fold1/test.txt -validate bin/MQ2008/Fold1/vali.txt -ranker 6 -metric2t NDCG@10 -metric2T ERR@10 -save mymodel.txt".split(" ")
-
+train_query = "java -jar bin/RankLib-2.15.jar -train own/iter3/train.txt -test own/iter3/test.txt -validate own/iter3/validate.txt -ranker 1 -metric2t MAP@10 -save map10baseline3.txt -epoch 200"
+individual_query_scores = "java -jar bin/RankLib-2.15.jar -load map10baseline3.txt -test own/iter3/test.txt -metric2T MAP@10 -idv output/map10iter3_rn100.txt"
