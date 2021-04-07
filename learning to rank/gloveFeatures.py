2dab1589674c26d47d416e2aa033d53bdfc1b535
Marijn Veelers <r.f.m.veelers@student.tudelft.nl>
2021-04-07 14:30:43 +0200

    Created baseline features + Glove & fastText features

--- learning to rank/gloveFeatures.py
+++ learning to rank/gloveFeatures.py
@@ -1,19 +1,18 @@
 from whoosh.qparser import QueryParser
-
-gloveFile = "C:/Users/Rozemarijn Veelers/Documents\Master/Information Retrieval/Core IR/information_retrieval/data/vectors.txt"
-
+from whoosh.index import create_in, open_dir
+from whoosh.fields import *
 import numpy as np
 import scipy.spatial.distance
 
-def create_feature_file():
+def create_features(indexQ, indexP, outputfile, readfile):
     schemaQ = Schema(title=TEXT(stored=True), content=TEXT(stored=True))
-    ixQ = open_dir('../index/qvectorindex', schema=schemaQ)
+    ixQ = open_dir('../index/' + indexQ, schema=schemaQ)
     schemaP = Schema(title=TEXT(stored=True), content=TEXT(stored=True))
-    ixP = open_dir('../index/pvectorindex', schema=schemaP)
+    ixP = open_dir('../index/' + indexP, schema=schemaP)
 
     i = 0
-    feature_file = open("output/glove_feature.txt", "a", newline='')
-    for line in list(open("../data/2019qrels-pass.txt", encoding='utf8')):
+    feature_file = open("../featuresets/" + outputfile, "a", newline='')
+    for line in list(open("../data/" + readfile, encoding='utf8')):
         queryID = line.split(' ')[0]
         passageID = line.split(' ')[2]
         relevance = int(line.split(' ')[3])
@@ -26,14 +25,14 @@ def create_feature_file():
 
         with ixQ.searcher() as s:
             results = s.search(q)
-            qvector = list(results[0].get('content'))
+            qvector = np.array(list(map(float, results[0].get('content').strip('[').strip(']').split(', '))))
 
         with ixP.searcher() as s:
             results = s.search(p)
-            pvector = list(results[0].get('content'))
+            pvector = np.array(list(map(float, results[0].get('content').strip('[').strip(']').split(', '))))
 
-        euclidian = np.linalg.norm(qvector - pvector)
         cosine = scipy.spatial.distance.cosine(qvector, pvector)
+        euclidian = np.linalg.norm(qvector - pvector)
 
         if i % 10 == 0:
             print(i)
@@ -45,3 +44,9 @@ def create_feature_file():
         feature_file.write('qid:' + str(queryID) + ' pid:' + str(passageID) + ' 1:' + str(cosine) + ' 2:' + str(euclidian) + '\n')
     feature_file.close()
 
+if __name__ == '__main__':
+    indexQ = "fastqvector-index"
+    indexP = "fastpvector-index"
+    outputfile = "fast_features.txt"
+    readfile = "2019qrels-pass.txt"
+    create_features(indexQ, indexP, outputfile, readfile)
