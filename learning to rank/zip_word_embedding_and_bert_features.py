4abcc6430e07e369030d6539f41cae04a8904449
Rolf de Vries <rolf_d_v@hotmail.com>
2021-04-08 22:58:17 +0200

    Created script and file to combine baseline, glove and fasttext features with bert features

--- learning to rank/zip_word_embedding_and_bert_features.py
+++ learning to rank/zip_word_embedding_and_bert_features.py
@@ -29,30 +29,23 @@ if __name__ == '__main__':
         for key in baseline_glove_fasttext_features_training_by_qid.keys():
             if key in bert_features_by_qid.keys():
                 baseline_lines = baseline_glove_fasttext_features_training_by_qid[key]
-                glove_lines = bert_features_by_qid[key]
+                bert_lines = bert_features_by_qid[key]
 
-                baseline_lines_sorted = sorted(baseline_lines, key=lambda x: int(x.split(" ")[5].replace("#", "")))
-                glove_lines_sorted = sorted(glove_lines, key=lambda x: int(x.split(" ")[1].replace("pid:", "")))
-                assert len(baseline_lines_sorted) == len(glove_lines_sorted)
-                zipped = zip(baseline_lines_sorted, glove_lines_sorted)
+                baseline_lines_sorted = sorted(baseline_lines, key=lambda x: int(x.split(" ")[-1].replace("#", "")))
+                bert_lines_sorted = sorted(bert_lines, key=lambda x: int(x.split(" ")[1].replace("pid:", "")))
+                assert len(baseline_lines_sorted) == len(bert_lines_sorted)
+                zipped = zip(baseline_lines_sorted, bert_lines_sorted)
                 for combined_line in zipped:
                     baseline_line = combined_line[0]
-                    glove_line = combined_line[1]
+                    bert_line = combined_line[1]
                     baseline_pid = baseline_line.split(" ")[-1].replace("#", "")
-                    glove_pid = glove_line.split(" ")[1].replace("pid:", "")
-                    assert baseline_pid == glove_pid
+                    bert_pid = bert_line.split(" ")[1].replace("pid:", "")
+                    assert baseline_pid == bert_pid
 
                     new_line_split = baseline_line.split(" ")[:-1]
 
-                    glove_cosine_param = glove_line.split(" ")[2].replace("1:", "4:")
-                    glove_euclidian_param = glove_line.split(" ")[3].replace("2:", "5:")
-                    new_line_split.append(glove_cosine_param)
-                    new_line_split.append(glove_euclidian_param)
-
-                    fasttext_cosine_param = glove_line.split(" ")[2].replace("1:", "6:")
-                    fasttext_euclidian_param = glove_line.split(" ")[3].replace("2:", "7:")
-                    new_line_split.append(fasttext_cosine_param)
-                    new_line_split.append(fasttext_euclidian_param)
+                    for i, val in enumerate(bert_line.split(" ")[2:]):
+                        new_line_split.append(val.replace(f"{i + 1}:", f"{i + 8}:"))
 
                     new_line_split.append(f"#{baseline_pid}")
                     res_file.write(f"{' '.join(new_line_split)}\n")
